440fa56a4420e47f3a1a0fa56cbf622e018457e7
Ajlloyd <ajlloyd98@gmail.com>
2019-07-14 18:23:16 +0100

    test commit 5 (tSNE Finished)

--- PCA.py
+++ PCA.py
@@ -4,6 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from scipy.linalg import svd
 from sklearn.model_selection import train_test_split
 from numpy.linalg import eig
+from sklearn.manifold import TSNE
 
 
 from sklearn.datasets import load_iris
@@ -11,9 +12,10 @@ data = load_iris()
 y = data.target
 x = data.data
 
+print(data)
 
 
-#PCA plot:
+
 def plot_PCA(xs, ys):
     scaler = StandardScaler()
     X_std = scaler.fit_transform(xs)
@@ -23,9 +25,33 @@ def plot_PCA(xs, ys):
     z = vectors.T.dot(X_std.T).T
     PCA1 = z[:,0]
     PCA2 = z[:,1]
-    plt.plot(PCA1[ys==0],PCA2[ys==0],"g.")
-    plt.plot(PCA1[ys==1],PCA2[ys==1],"b.")
-    plt.plot(PCA1[ys==2],PCA2[ys==2],"r.")
+
+    ax = plt.subplot(111)
+    ax.plot(PCA1[ys==0],PCA2[ys==0],"g.",label="Iris-Setosa")
+    ax.plot(PCA1[ys==1],PCA2[ys==1],"b.", label="Iris-Versicolour")
+    ax.plot(PCA1[ys==2],PCA2[ys==2],"r.", label="Iris-Virginica")
+    ax.legend()
+    plt.xlabel("PCA1")
+    plt.ylabel("PCA2")
+    plt.show()
+plot_PCA(x,y)
+
+
+def plot_tSNE(xs,ys):
+    scaler = StandardScaler()
+    X_std = scaler.fit_transform(xs)
+    tSNE = TSNE(n_components=2)
+    z = tSNE.fit_transform(X_std)
+    tSNE1 = z[:,0]
+    tSNE2 = z[:,1]
+
+    ax = plt.subplot(111)
+    ax.plot(tSNE1[ys==0],tSNE2[ys==0],"g.",label="Iris-Setosa")
+    ax.plot(tSNE1[ys==1],tSNE2[ys==1],"b.", label="Iris-Versicolour")
+    ax.plot(tSNE1[ys==2],tSNE2[ys==2],"r.", label="Iris-Virginica")
+    ax.legend()
+    plt.xlabel("tSNE1")
+    plt.ylabel("tSNE2")
     plt.show()
 
-plot_PCA(x,y)
+plot_tSNE(x,y)
